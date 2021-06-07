5468f3f735fdf197d50b0b94a916dad2c5991e30
Grace Mugoiri <gracemugoiri@gmail.com>
2021-06-07 12:59:03 +0300

    sends money and resets game test

--- test/Lottery.test.js
+++ test/Lottery.test.js
@@ -79,5 +79,24 @@ describe('Lottery Contract', () => {
 			assert(error)
 		}
 	});
+
+	it('it sends money to the winner and resets players array', async () => {
+		await lottery.methods.enterLottery().send({
+			from: accounts[0],
+			value: web3.utils.toWei('2', 'ether')
+		});
+
+		const initialBalance = await web3.eth.getBalance(accounts[0]);
+
+		await lottery.methods.pickWinner().send({
+			from: accounts[0]
+		});
+
+		const finalBalance = await web3.eth.getBalance(accounts[0]);
+
+		const difference = finalBalance - initialBalance;
+		assert(difference > web3.utils.toWei('1.8', 'ether'));
+		
+	});
 });
 
