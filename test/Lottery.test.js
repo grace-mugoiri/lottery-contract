6d5590f5b588c3f7074429777f32b7d0ea66279a
Grace Mugoiri <gracemugoiri@gmail.com>
2021-06-07 12:07:11 +0300

    deploys a contract test passed

--- test/Lottery.test.js
+++ test/Lottery.test.js
@@ -11,8 +11,14 @@ let accounts;
 beforeEach(async () => {
 	accounts = await web3.eth.getAccounts();
 	
-	lottery = await web3.eth.Contract(JSON.parse(interface))
+	lottery = await new web3.eth.Contract(JSON.parse(interface))
 	.deploy({ data: bytecode})
 	.send({ from: accounts[0], gas: '1000000'})
-})
+});
+
+describe('Lottery Contract', () => {
+	it('deploys a contract', () => {
+		assert.ok(lottery.options.address);
+	});
+});
 
